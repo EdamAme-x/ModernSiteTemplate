04b0dc000804fe5936335f3b9f79c3e7526e3649
EdamAme-x <edame8080@gmail.com>
2023-10-23 12:32:24 +0900

    Init on Flowbite

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
+import { Layout } from './components/layout/Layout'
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <Layout></Layout>
+    </>
   )
 }
